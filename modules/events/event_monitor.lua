da779061e77c8f099b3f80709ab61806f911fd75
Expotality <wyatt.x909@gmail.com>
2026-09-07 12:51:01 -0700

    Add event_monitor.lua for client-side activity monitoring

---
+++ modules/events/event_monitor.lua
@@ -0,0 +1,13 @@
+return {
+    Name = "Event Monitor",
+
+    Type = "Action",
+
+    Description = "Monitor client-side remote activity.",
+
+    Open = function(self)
+
+        -- Event monitor will be opened here
+
+    end
+}
