0238e2799f0a6a74df29e34eed73ef4f1e961142
Expotality <wyatt.x909@gmail.com>
2026-09-06 19:48:05 -0700

    Update manifest.lua

--- manifest.lua
+++ manifest.lua
@@ -1,1 +1,43 @@
+return {
+    {
+        Name = "ESP",
+        File = "modules/visuals/esp.lua",
+        Tab = "Visuals",
+    },
 
+    {
+        Name = "Speed",
+        File = "modules/movement/speed.lua",
+        Tab = "Movement",
+    },
+
+    {
+        Name = "Flight",
+        File = "modules/movement/flight.lua",
+        Tab = "Movement",
+    },
+
+    {
+        Name = "NoClip",
+        File = "modules/movement/noclip.lua",
+        Tab = "Movement",
+    },
+
+    {
+        Name = "Events",
+        File = "modules/events/debugger.lua",
+        Tab = "Events",
+    },
+
+    {
+        Name = "Customization",
+        File = "modules/customization/customization.lua",
+        Tab = "Customization",
+    },
+
+    {
+        Name = "Utilities",
+        File = "modules/utilities/example.lua",
+        Tab = "Utilities",
+    },
+}
